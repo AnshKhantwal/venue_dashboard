778ae01a904e4dd93e56314effc31b9009a9234b
AnshKhantwal <anshkhantwal04@gmail.com>
2025-07-20 12:41:33 +0530

    Updates

--- venue-booking-backend/db.js
+++ venue-booking-backend/db.js
@@ -1,18 +1,40 @@
-// db.js
+// // db.js
+// const mysql = require('mysql2');
+// const db = mysql.createConnection({
+// host: 'localhost',
+// user: 'root',
+// password: 'root', // your MySQL password
+// database: 'venue_booking'
+// });
+
+// db.connect(err => {
+// if (err) {
+// console.error('DB connection error:', err);
+// } else {
+// console.log('MySQL connected');
+// }
+// });
+
+// module.exports = db;
+
+require('dotenv').config(); // load .env file
+
 const mysql = require('mysql2');
+
 const db = mysql.createConnection({
-host: 'localhost',
-user: 'root',
-password: 'root', // your MySQL password
-database: 'venue_booking'
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT, // important: Railway uses a custom port
 });
 
 db.connect(err => {
-if (err) {
-console.error('DB connection error:', err);
-} else {
-console.log('MySQL connected');
-}
+  if (err) {
+    console.error('DB connection error:', err);
+  } else {
+    console.log('MySQL connected');
+  }
 });
 
 module.exports = db;
--- venue-booking-backend/server.js
+++ venue-booking-backend/server.js
@@ -2,12 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const adminRoutes = require('./routes/admin'); 
+
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
 app.use('/api/admin', adminRoutes); // All /admin routes handled here
 
-app.listen(5000, () => {
-  console.log('Server running on http://localhost:5000');
+// app.listen(5000, () => {
+//   console.log('Server running on http://localhost:5000');
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
